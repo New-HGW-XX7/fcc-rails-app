d061f48e652221e66b8e7a3e8c46bdd57d0b7336
HGWXX7 <new_hgw_xx7@protonmail.com>
2023-10-21 13:54:08 +0200

    Implement CRUD scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :friends
   root to: 'home#index'
 
   get '/home/about', to: 'home#about'
+
 
   # get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
